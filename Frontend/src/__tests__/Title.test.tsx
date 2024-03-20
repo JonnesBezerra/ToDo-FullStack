da4c08ba66735634ba8d9c6fa145794e52e9f092
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-03-20 00:59:56 -0300

    refact: test title

--- Frontend/src/__tests__/Title.test.tsx
+++ Frontend/src/__tests__/Title.test.tsx
@@ -1,11 +1,12 @@
 import { Title } from "@/components";
 import Home from "@/app/page";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 test("displays Title", () => {
   render(<Home />);
 
   const title = screen.getByRole("title");
 
-  expect(title).toHaveBeenCalled();
+  expect(title).toBeInTheDocument();
 });
