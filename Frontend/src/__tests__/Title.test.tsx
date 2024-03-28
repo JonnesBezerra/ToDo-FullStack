bfb55500343c8d5ed486139f14ecaf0204ef1279
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-03-28 00:23:02 -0300

    refact: improve Title test description

--- Frontend/src/__tests__/Title.test.tsx
+++ Frontend/src/__tests__/Title.test.tsx
@@ -1,12 +1,13 @@
-import { Title } from "@/components";
 import Home from "@/app/page";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("displays Title", () => {
-  render(<Home />);
+describe("Title component", () => {
+  it("displays Title", () => {
+    render(<Home />);
 
-  const title = screen.getByRole("title");
+    const title = screen.getByRole("title");
 
-  expect(title).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  });
 });
