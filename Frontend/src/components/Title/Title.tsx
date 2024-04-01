b0a51d86a1c0690cedf0e46bf8390c7e425e4fe6
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-01 18:35:29 -0300

    feat: check icon on title

--- Frontend/src/components/Title/Title.tsx
+++ Frontend/src/components/Title/Title.tsx
@@ -1,10 +1,12 @@
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 const Title = () => {
   return (
-    <div>
-      <h1 role="title" className="text-slate-900 text-5xl">
-        Todo App
+    <div className="flex items-center">
+      <CheckCircleIcon className="text-slate-900 h-12 mr-1 hover:transform hover:scale-150 hover:duration-1000 duration-500 animate-spin-once" />
+      <h1 role="title" className="text-slate-900 text-4xl font-bold">
+        TodoApp
       </h1>
     </div>
   );
