0f497489eccc0f870c87f01b662dd4f142aa5578
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-02 21:56:43 -0300

    feat: tasks component receive dynamic content

--- Frontend/src/components/Task/Task.tsx
+++ Frontend/src/components/Task/Task.tsx
@@ -1,11 +1,20 @@
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
-const Task = () => {
+interface Props {
+  _id: string;
+  description: string;
+  completed: boolean;
+}
+
+const Task = ({ _id, description, completed }: Props) => {
   return (
-    <li className="bg-white bg-opacity-50 rounded-lg px-2 py-3 flex gap-1 text-slate-900">
+    <li
+      id={_id}
+      className="bg-white bg-opacity-50 rounded-lg px-2 py-3 flex gap-1 text-slate-900"
+    >
       <CheckCircleIcon className="text-slate-900 h-6" />
-      task 1
+      {description}
     </li>
   );
 };
--- Frontend/src/components/Tasks/Tasks.tsx
+++ Frontend/src/components/Tasks/Tasks.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 import { Task } from "..";
 
+const todo = [
+  {
+    _id: "65ea7111922a2b5ccd89c7ac",
+    description: "Write MongoDB code",
+    completed: false,
+    __v: 0,
+  },
+  {
+    _id: "65ea7121922a2b5ccd89c7af",
+    description: "Write JavaScript code",
+    completed: false,
+    __v: 0,
+  },
+  {
+    _id: "65ea712d922a2b5ccd89c7b1",
+    description: "Write Python code",
+    completed: false,
+    __v: 0,
+  },
+];
+
 const Tasks = () => {
   return (
     <section className="flex flex-1 flex-col w-full my-4">
       <h2 className="font-bold text-slate-900 text-2xl mt-5">Tasks</h2>
       <ul className="flex flex-col gap-2 mt-5">
-        <Task />
-        <Task />
-        <Task />
+        {todo.map((item) => (
+          <Task
+            _id={item._id}
+            description={item.description}
+            completed={item.completed}
+            key={item._id}
+          />
+        ))}
       </ul>
     </section>
   );
