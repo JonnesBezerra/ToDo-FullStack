1aec568c6582c5964e05684b9dc242b9bc0ed386
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-02 21:36:48 -0300

    style: improve task style

--- Frontend/src/components/Task/Task.tsx
+++ Frontend/src/components/Task/Task.tsx
@@ -1,7 +1,13 @@
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 const Task = () => {
-  return <li className="">task 1</li>;
+  return (
+    <li className="bg-white bg-opacity-50 rounded-lg px-2 py-3 flex gap-1 text-slate-900">
+      <CheckCircleIcon className="text-slate-900 h-6" />
+      task 1
+    </li>
+  );
 };
 
 export default Task;
