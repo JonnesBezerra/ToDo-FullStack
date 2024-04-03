989e4f5c01c664a294d715f18dc0c52592f50bf6
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-02 21:26:22 -0300

    feat: Task component

---
+++ Frontend/src/components/Task/Task.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Task = () => {
+  return <li className="">task 1</li>;
+};
+
+export default Task;
--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -1,5 +1,7 @@
 import InputTask from "./InputTask/InputTask";
 import GradientBack from "./GradientBack/GradientBack";
+import Tasks from "./Tasks/Tasks";
+import Task from "./Task/Task";
 import Title from "./Title/Title";
 
-export { InputTask, GradientBack, Title };
+export { InputTask, GradientBack, Tasks, Task, Title };
