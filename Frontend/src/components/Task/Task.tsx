b34e207a58ebaba89f4fe22aa61db0a55a630b97
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-11 23:14:35 -0300

    feat: pass on render task correctly test

--- Frontend/src/components/Task/Task.tsx
+++ Frontend/src/components/Task/Task.tsx
@@ -13,7 +13,10 @@ const Task = ({ _id, description, completed }: Props) => {
       id={_id}
       className="bg-white bg-opacity-50 rounded-lg px-2 py-3 flex gap-1 text-slate-900"
     >
-      <CheckCircleIcon className="text-slate-900 h-6" />
+      <CheckCircleIcon
+        className="text-slate-900 h-6"
+        data-testid="check-outline"
+      />
       {description}
     </li>
   );
