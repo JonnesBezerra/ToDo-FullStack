9616e83322713f38b1338331ee4ca76544307086
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-12 01:38:01 -0300

    feat: while fetching task status display loading component

--- Frontend/src/components/Task/Task.tsx
+++ Frontend/src/components/Task/Task.tsx
@@ -1,29 +1,56 @@
-import { CheckCircleIcon } from "@heroicons/react/24/outline";
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { getTasks, updateStatusTask } from "@/server";
+import { CheckCircleIcon as CheckOutline } from "@heroicons/react/24/outline";
+import { CheckCircleIcon as CheckSolid } from "@heroicons/react/24/solid";
+import LoadingTask from "../LoadingTask/LoadingTask";
 
-interface Props {
+interface TaskProps {
   _id: string;
   description: string;
   completed: boolean;
+  setTasks: Function;
 }
 
-const Task = ({ _id, description, completed }: Props) => {
+const Task = ({ _id, description, completed, setTasks }: TaskProps) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    await updateStatusTask(_id, { completed: !completed });
+    await getTasks().then((data) => {
+      setTasks(data);
+      setLoading(false);
+    });
+  };
+
+  if (loading) {
+    return <LoadingTask />;
+  }
+
   return (
-    <li
-      id={_id}
-      className="bg-white bg-opacity-50 rounded-lg px-2 py-3 flex gap-1 text-slate-900"
-    >
-      <CheckCircleIcon
-        className="text-slate-900 h-6"
-        data-testid="check-outline"
-      />
+    <li id={_id} className="bg-white bg-opacity-50 rounded-lg px-2 py-3 flex">
+      <div
+        className="flex gap-1 cursor-pointer"
+        onClick={async () => handleClick()}
+      >
+        {completed ? (
+          <CheckSolid
+            className="text-slate-900 h-6"
+            data-testid="check-solid"
+          />
+        ) : (
+          <CheckOutline
+            className="text-slate-900 h-6"
+            data-testid="check-outline"
+          />
         )}
         <p
           className={`text-slate-900 ${
             completed ? "line-through" : "no-underline"
           }`}
         >
-      {description}
+          {description}
         </p>
       </div>
     </li>
