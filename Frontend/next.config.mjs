faa115331347f1b9710cd297cf3c10ac04da75c1
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2024-04-29 23:44:28 -0300

    chore: original next.config.mjs

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  // basePath: "/ToDoApp-FullStack/Frontend",
-};
+const nextConfig = {};
 
 export default nextConfig;
